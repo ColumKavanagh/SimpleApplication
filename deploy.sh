2b32cdfaf60e38f4a66cc1db68bbc6b43331223d
Colum Kavanagh <colum.j.kavanagh@gmail.com>
2022-03-25 11:37:10 +0000

    add deploy.sh file

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+sudo apt update
+sudo apt install nodejs 
+sudo apt install npm
+# Install pm2 which is a production process manager for Node.js with a built-in load balancer.
+sudo npm install -g pm2
+# Stop any instance of our application running currently
+pm2 stop SimpleApplication
+# Change directory into folder where application is downloaded
+cd SimpleApplication
+# Install application dependencies
+npm install
+# Start the application with the process name ExampleApplication
+pm2 start ./bin/www --name SimpleApplication
